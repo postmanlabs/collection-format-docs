4619f8d1254dcdb73e180841a02c878c63fd24db
Mindi Weik <mindi.weik@postman.com>
2025-09-03 17:53:08 -0600

    [c] install Transcend

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,9 @@
 const React = require('react');
 const bff = require('./bff.json');
 
+const getEnvironment = () => process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'production';
+const transcendEnv = getEnvironment() === 'production' ? 'cm' : 'cm-test';
+
 exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents();
   const modifiedComponents = [...headComponents];
@@ -11,6 +14,14 @@ exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
       dangerouslySetInnerHTML: {
         __html: bff.script,
       },
+    }),
+    React.createElement('script', {
+      key: 'transcend',
+      'data-px': '<!-- Transcend -->',
+      dnt: true,
+      src: `https://transcend-cdn.com/${transcendEnv}/1f0cf102-c592-4eb4-834f-e07f2fe68ef1/airgap.js`,
+      'data-cfasync': false,
+      'data-local-sync': 'allow-network-observable',
     }),
   );
 
