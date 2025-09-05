0e770e6c27acbc53744d17deb65b9c494b9aeb82
Mindi Weik <mindi.weik@postman.com>
2025-09-05 15:23:31 -0600

    adjust based on other patterns

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,7 @@
 const React = require('react');
 const bff = require('./bff.json');
 
-const getEnvironment = () => process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'production';
-const transcendEnv = getEnvironment() === 'production' ? 'cm' : 'cm-test';
+const transcendEnv = (process.env.GATSBY_ACTIVE_ENV === 'production') || (process.env.NODE_ENV === 'production') ? 'cm' : 'cm-test';
 
 exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents();
@@ -15,14 +14,17 @@ exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
         __html: bff.script,
       },
     }),
+  );
+
+  modifiedComponents.push(
     React.createElement('script', {
       key: 'transcend',
       'data-px': '<!-- Transcend -->',
-      dnt: true,
       src: `https://transcend-cdn.com/${transcendEnv}/1f0cf102-c592-4eb4-834f-e07f2fe68ef1/airgap.js`,
-      'data-cfasync': false,
+      'data-cfasync': 'false',
       'data-local-sync': 'allow-network-observable',
-    }),
+      async: true
+    })
   );
 
   replaceHeadComponents(modifiedComponents);
