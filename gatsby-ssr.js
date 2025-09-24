b6944de3727c7fb62dd0334c470a7c42217f60fd
Mindi Weik <mindi.weik@postman.com>
2025-09-24 15:24:41 -0600

    add transcend to gatsby-ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,6 +5,18 @@ exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents();
   const modifiedComponents = [...headComponents];
 
+  // Transcend is our data privacy and consent manager.
+  // It must load first on the page.
+  modifiedComponents.push(
+    React.createElement('script', {
+      key: "airgap",
+      src: "https://transcend-cdn.com/cm/1f0cf102-c592-4eb4-834f-e07f2fe68ef1/airgap.js",
+      'data-cfasync': false,
+      'data-local-sync': "allow-network-observable",
+      async: true
+    })
+  );
+
   modifiedComponents.push(
     React.createElement('script', {
       key: 'pm',
